56b0906a9e964bfeadfaf99bd51b10ca13a07a0b
clarinovist <nugroho.pramono@gmail.com>
2026-04-02 21:24:54 +0700

    fix(lint): ignore no-require-imports in query scripts

--- query-tenant.js
+++ query-tenant.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 prisma.tenant.findMany().then(t => console.log(t)).catch(console.error).finally(()=>prisma.$disconnect())
